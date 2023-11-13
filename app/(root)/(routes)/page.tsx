6bd785cb718f2567503bcf7642c16f7909b9dbc2
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-13 13:57:00 +1300

    root page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function RootPage() {
-  return <div className="text-green-500 text-3xl">RootPage(protected)</div>
+  return (
+    <div className="text-green-500 text-3xl">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  )
 }
