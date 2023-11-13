3bfd4547dab4c247d6fb2eae5cdfdb2c1419d48e
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-13 18:02:22 +1300

    added categories bar

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,13 @@
+import Categories from '@/components/Categories'
 import SearchInput from '@/components/SearchInput'
+import prismadb from '@/lib/prismadb'
 
-export default function RootPage() {
+export default async function RootPage() {
+  const categories = await prismadb.category.findMany()
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
