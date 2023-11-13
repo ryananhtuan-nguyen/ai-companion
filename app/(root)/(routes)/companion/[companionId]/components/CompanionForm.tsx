610ca3fd5ee5e70d352bc19f25f0069f8f3e9aa2
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-13 18:18:45 +1300

    actual form for companions

---
+++ app/(root)/(routes)/companion/[companionId]/components/CompanionForm.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Category, Companion } from '@prisma/client'
+
+interface CompanionFormProps {
+  initialData: Companion | null
+  categories: Category[]
+}
+
+const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
+  return <div>CompanionForm</div>
+}
+
+export default CompanionForm
