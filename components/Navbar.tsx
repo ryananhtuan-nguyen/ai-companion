24974d24b2112a8c6dfa47c6ff3b920f3e4fb715
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-13 13:57:08 +1300

    making navbars

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { UserButton } from '@clerk/nextjs'
+import { Menu } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Link from 'next/link'
+
+const font = Poppins({
+  weight: '600',
+  subsets: ['latin'],
+})
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              'hidden md:block text-xl md:text-3xl font-bold text-primary',
+              font.className
+            )}
+          >
+            companion.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <UserButton />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
