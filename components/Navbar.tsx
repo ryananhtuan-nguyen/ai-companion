9c34119216621b551f4f3de7153af59b68d1fd4e
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-13 14:06:14 +1300

    added button variants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,10 @@
 
 import { cn } from '@/lib/utils'
 import { UserButton } from '@clerk/nextjs'
-import { Menu } from 'lucide-react'
+import { Menu, Sparkle } from 'lucide-react'
 import { Poppins } from 'next/font/google'
 import Link from 'next/link'
+import { Button } from './ui/button'
 
 const font = Poppins({
   weight: '600',
@@ -28,6 +29,10 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkle className="h-4 w-4 fill-white text-white ml-2" />
+        </Button>
         <UserButton />
       </div>
     </div>
