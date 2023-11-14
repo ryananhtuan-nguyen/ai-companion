c699c378d681208d4a618d56e4fab006af496848
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-14 15:19:44 +1300

    fixed bug

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,15 +1,20 @@
 'use client'
 
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { CldUploadButton } from 'next-cloudinary'
-import Image from 'next/image'
+
 interface ImageUploadProps {
   value: string
   onChange: (src: string) => void
   disabled?: boolean
 }
 
-const ImageUpload = ({ value, onChange, disabled }: ImageUploadProps) => {
+export const ImageUpload = ({
+  value,
+  onChange,
+  disabled,
+}: ImageUploadProps) => {
   const [isMounted, setIsMounted] = useState(false)
 
   useEffect(() => {
@@ -17,23 +22,36 @@ const ImageUpload = ({ value, onChange, disabled }: ImageUploadProps) => {
   }, [])
 
   if (!isMounted) {
-    return null
+    return false
   }
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
       <CldUploadButton
         options={{ maxFiles: 1 }}
+        onUpload={(result: any) => onChange(result.info.secure_url)}
         uploadPreset="messenger"
-        onUpload={(result: any) => {
-          onChange(result.info.secure_url)
-        }}
       >
-        <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+        <div
+          className="
+            p-4 
+            border-4 
+            border-dashed
+            border-primary/10 
+            rounded-lg 
+            hover:opacity-75 
+            transition 
+            flex 
+            flex-col 
+            space-y-2 
+            items-center 
+            justify-center
+          "
+        >
           <div className="relative h-40 w-40">
             <Image
               fill
-              alt="upload"
+              alt="Upload"
               src={value || '/placeholder.svg'}
               className="rounded-lg object-cover"
             />
@@ -43,5 +61,3 @@ const ImageUpload = ({ value, onChange, disabled }: ImageUploadProps) => {
     </div>
   )
 }
-
-export default ImageUpload
