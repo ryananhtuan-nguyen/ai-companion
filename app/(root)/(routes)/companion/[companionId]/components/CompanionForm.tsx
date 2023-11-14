9a524c09b751164780e7111c9ab145bb9ab509ed
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-14 13:59:44 +1300

    basic form to create companions

--- app/(root)/(routes)/companion/[companionId]/components/CompanionForm.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/CompanionForm.tsx
@@ -2,6 +2,18 @@
 
 import * as z from 'zod'
 import { Category, Companion } from '@prisma/client'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Separator } from '@/components/ui/separator'
+import ImageUpload from '@/components/ImageUpload'
 
 interface CompanionFormProps {
   initialData: Companion | null
@@ -22,7 +34,70 @@ const formSchema = z.object({
 })
 
 const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
-  return <div>CompanionForm</div>
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      description: '',
+      instructions: '',
+      seed: '',
+      src: '',
+      categoryId: undefined,
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10"
+        >
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General information about your Companion
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="src"
+            render={({ field }) => (
+              <FormItem className="flex flex-col items-center justify-center space-y-4">
+                <FormControl>
+                  <ImageUpload
+                    disabled={isLoading}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel></FormLabel>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
+    </div>
+  )
 }
 
 export default CompanionForm
