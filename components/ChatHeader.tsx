185becca2db01cde2d666534accfef57c8819ed9
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-15 13:43:10 +1300

    edit/delete

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -1,8 +1,24 @@
 'use client'
 import { Companion, Message } from '@prisma/client'
 import { Button } from './ui/button'
-import { ChevronLeft } from 'lucide-react'
+import {
+  ChevronLeft,
+  Edit,
+  MessageSquare,
+  MoreVertical,
+  Trash,
+} from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import BotAvatar from './BotAvatar'
+import { useUser } from '@clerk/nextjs'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { useToast } from './ui/use-toast'
+import axios from 'axios'
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -15,6 +31,24 @@ interface ChatHeaderProps {
 
 const ChatHeader = ({ companion }: ChatHeaderProps) => {
   const router = useRouter()
+  const { toast } = useToast()
+
+  const { user } = useUser()
+
+  const onDelete = async () => {
+    try {
+      await axios.delete(`/api/companions/${companion.id}`)
+
+      toast({
+        description: 'Success.',
+      })
+    } catch (error) {
+      toast({
+        description: 'Something went wrong',
+        variant: 'destructive',
+      })
+    }
+  }
 
   return (
     <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
@@ -22,7 +56,41 @@ const ChatHeader = ({ companion }: ChatHeaderProps) => {
         <Button size="icon" variant="ghost" onClick={() => router.back()}>
           <ChevronLeft className="h-8 w-8" />
         </Button>
+        <BotAvatar src={companion.src} />
+        <div className="flex flex-col gap-y-1">
+          <div className="flex items-center gap-x-2">
+            <p className=" font-bold">{companion.name}</p>
+            <div className="flex items-center text-xs text-muted-foreground">
+              <MessageSquare className="w-3 h-3 mr-1" />
+              {companion._count.messages}
+            </div>
+          </div>
+          <p className="text-xs text-muted-foreground">
+            Created by {companion.userName}
+          </p>
+        </div>
       </div>
+      {user?.id === companion.userId && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon">
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => router.push(`/companion/${companion.id}`)}
+            >
+              <Edit className="w-4 h-4 mr-2" />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={onDelete}>
+              <Trash className="w-4 h-4 mr-2" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </div>
   )
 }
