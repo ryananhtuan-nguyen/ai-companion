17684703b57048f8c066f2dbc2ab52e23c54a3f3
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-16 19:45:00 +1300

    protect route required Pro plan

--- app/api/companion/route.ts
+++ app/api/companion/route.ts
@@ -1,4 +1,5 @@
 import prismadb from '@/lib/prismadb'
+import { checkSubscription } from '@/lib/subscription'
 import { currentUser } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
@@ -25,7 +26,12 @@ export async function POST(req: Request) {
       return new NextResponse('Missing required fields', { status: 400 })
     }
 
-    //TODO: Check for subscription
+    //Check for subscription
+    const isPro = await checkSubscription()
+
+    if (!isPro) {
+      return new NextResponse('Pro subscription required', { status: 403 })
+    }
 
     const companion = await prismadb.companion.create({
       data: {
