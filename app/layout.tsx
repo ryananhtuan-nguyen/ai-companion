d1c194011f73d1fb95ff56e0d5e7fc85a155a3ce
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-16 18:46:43 +1300

    add pro button

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
 import { Toaster } from '@/components/ui/toaster'
+import ProModal from '@/components/ProModal'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn('bg-secondary', inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <ProModal />
             {children}
             <Toaster />
           </ThemeProvider>
