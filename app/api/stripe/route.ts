49b4a78e38cddfae921b71bdde54c1b23c51cef4
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-16 18:09:31 +1300

    stripe session

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from 'next/server'
 import prismadb from '@/lib/prismadb'
 import { stripe } from '@/lib/stripe'
 import { absoluteUrl } from '@/lib/utils'
-import { metadata } from '@/app/layout'
 
 const settingsUrl = absoluteUrl('/settings')
 
