fccaf68d828018152d06d10e9b1c49f2f339ad38
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-16 19:44:48 +1300

    check Pro Plan

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,14 @@
 'use client'
 
+import { useProModal } from '@/hooks/use-pro-modal'
 import { cn } from '@/lib/utils'
 import { Home, Plus, Settings } from 'lucide-react'
 import { usePathname, useRouter } from 'next/navigation'
 
-const Sidebar = () => {
+const Sidebar = ({ isPro }: { isPro: boolean }) => {
   const pathname = usePathname()
   const router = useRouter()
+  const proModal = useProModal()
 
   const routes = [
     {
@@ -31,6 +33,9 @@ const Sidebar = () => {
 
   const onNavigate = (url: string, pro: boolean) => {
     //TODO: check if pro
+    if (pro && !isPro) {
+      return proModal.onOpen()
+    }
 
     return router.push(url)
   }
